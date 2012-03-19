b3683725c7b08a08cb22cb73c054068cc13a3a1d
Varun Pai <varunpai12@gmail.com>
2012-03-18 22:00:02 -0700

    adding rails stuff

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @loc = @user.location
+ 
+    if(@loc.nil?)
+      @loc = Location.new(params[:location])
+      @loc.user_id = @user.id
+      @user.location = @loc
+    else
+      Location.destroy(@loc.id)
+      @newLoc = Location.new(params[:location])
+      @newLoc.user_id = @user.id
+      @user.location = @newLoc
+    end
+ 
+    @users = User.all
+
+    respond_to do |format|
+        format.json { render :json => Location.all }
+    end
+  end
+
+  def showusers
+    @users = User.all
+    
+    respond_to do |format|
+        format.json { render :json => @users }
+    end
+  end
+
+  def showlocations
+    @locations = Location.all
+    
+    respond_to do |format|
+        format.json { render :json => @locations }
+    end
+  end
+
+  def create
+    @user = User.new()
+
+    respond_to do |format|
+      if @user.save
+        format.json  { render :json => @user }
+      else
+        @failure = 0
+        format.json  { render :json => @failure }
+      end
+    end
+  end
+ 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      if @user.save
+        format.json  { render :json => User.all }
+      else
+        @failure = 0
+        format.json  { render :json => @failure }
+      end
+    end
+  end
+
+end
---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunch::Application.config.secret_token = '9863f3f42f722c82ee18cbcf9f4e4623df9912d0307a4db312ce486a02734848933a53f00fcc7374053bd6b5d8053ec3eefb22fd97cfd1c733465d1e08815067'
